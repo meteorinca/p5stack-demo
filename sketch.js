a25942aaede419d69780e14989b56d1c214ad25d
Mj <mj@Moj-MacBook-Air.local>
2026-03-09 23:00:16 -0500

    Initial commit for full-screen p5 stackfalls sketch

--- sketch.js
+++ sketch.js
@@ -1,248 +1,248 @@
 const originalDat = [
-  0.5,
-  0.5,
-  217.113,
-  16.4414,
-  14.168,
-  16.9414,
-  181.059,
-  19.414099999999998,
-  28.4297,
-  36.3555,
-  55.668000000000006,
-  10.695300000000003,
-  158.977,
-  36.3555,
-  7.920999999999992,
-  25.7539,
-  36.5547,
-  47.0508,
-  42.98440000000001,
-  15.058599999999998,
-  31.9961,
-  62.1094,
-  144.4099,
-  8.714800000000004,
-  23.2891,
-  70.8242,
-  159.10590000000002,
-  48.25779999999999,
-  15.918,
-  119.082,
-  83.43360000000001,
-  106.715,
-  20.1875,
-  225.797,
-  71.4023,
-  13.973000000000013,
-  102.844,
-  119.082,
-  85.37100000000001,
-  32.206999999999994,
-  102.844,
-  151.289,
-  89.25400000000002,
-  33.762,
-  24.9766,
-  239.77,
-  14.613199999999996,
-  19.456999999999965,
-  47.6055,
-  239.77,
-  11.7851,
-  13.561999999999983,
-  64.5781,
-  239.77,
-  27.01169999999999,
-  20.635999999999996,
-  66.4609,
-  260.406,
-  21.2149,
-  41.01600000000002,
-  69.2891,
-  301.422,
-  6.367199999999997,
-  15.557999999999993,
-  79.4258,
-  301.422,
-  6.6015000000000015,
-  25.456999999999994,
-  64.5781,
-  316.98,
-  12.492199999999997,
-  4.949999999999989,
-  75.6563,
-  326.879,
-  13.4375,
-  9.430000000000007,
-  75.3047,
-  336.309,
-  6.308599999999998,
-  9.069999999999993,
-  84.3086,
-  336.309,
-  5.960899999999995,
-  8.553999999999974,
-  52.375,
-  253.332,
-  5.257800000000003,
-  15.425999999999988,
-  21.1602,
-  259.227,
-  27.351499999999998,
-  7.675000000000011,
-  20.1875,
-  266.902,
-  5.609400000000001,
-  7.883000000000038,
-  28.5781,
-  266.902,
-  5.718800000000002,
-  14.012,
-  38.7773,
-  266.902,
-  4.636800000000001,
-  10.199999999999989,
-  22.9922,
-  280.914,
-  17.331999999999997,
-  5.149000000000001,
-  27.1875,
-  286.063,
-  5.093800000000002,
-  27.351,
-  27.1875,
-  313.414,
-  4.468800000000002,
-  10.819999999999993,
-  34.8359,
-  286.063,
-  9.351599999999998,
-  10.355000000000018,
-  35.5313,
-  296.418,
-  7.882799999999996,
-  7.879000000000019,
-  105.641,
-  185.051,
-  13.077999999999989,
-  11.426000000000016,
-  104.254,
-  196.477,
-  4.358999999999995,
-  6.100999999999999,
-  109.813,
-  196.477,
-  4.959999999999994,
-  19.429000000000002,
-  106.434,
-  215.906,
-  10.968000000000004,
-  6.2309999999999945,
-  109.813,
-  222.137,
-  3.600999999999999,
-  6.61699999999999,
-  181.543,
-  185.051,
-  12.843999999999994,
-  19.719000000000023,
-  183.684,
-  204.77,
-  5.753999999999991,
-  14.252999999999986,
-  179.27,
-  219.023,
-  15.11699999999999,
-  8.622000000000014,
-  180.207,
-  227.645,
-  4.546999999999997,
-  7.889999999999986,
-  189.438,
-  227.645,
-  4.414000000000016,
-  6.420999999999992,
-  123.215,
-  185.051,
-  20.335999999999984,
-  52.625,
-  137.531,
-  237.676,
-  9.937999999999988,
-  11.238,
-  127.766,
-  237.676,
-  8.159999999999982,
-  24.215000000000032,
-  152.379,
-  185.051,
-  23.414000000000016,
-  68.81200000000001,
-  156.125,
-  253.863,
-  14.38300000000001,
-  14.382999999999981,
-  159.605,
-  268.246,
-  9.497000000000014,
-  10.367000000000019,
-  121.609,
-  261.891,
-  19.132999999999996,
-  16.72199999999998,
-  116.395,
-  278.613,
-  59.39800000000001,
-  10.702999999999975,
-  166.961,
-  289.316,
-  12.308999999999997,
-  21.00400000000002,
-  170.508,
-  310.32,
-  5.284999999999997,
-  12.843999999999994,
-  113.414,
-  289.316,
-  11.941000000000003,
-  10.5,
-  118.668,
-  299.816,
-  5.35199999999999,
-  13.715000000000032,
-  115.59,
-  313.531,
-  10.432999999999993,
-  6.555000000000007,
-  131.176,
-  289.316,
-  30.703000000000003,
-  38.12900000000002,
-  130.305,
-  327.445,
-  14.581999999999994,
-  9.899000000000001,
-  150.641,
-  327.445,
-  14.984000000000009,
-  9.899000000000001,
-  137.598,
-  337.344,
-  22.74199999999999,
-  9.764999999999986,
-  138.867,
-  347.109,
-  4.6839999999999975,
-  14.180000000000007,
-  147.469,
-  347.109,
-  3.975999999999999,
-  10.301000000000045,
-  153.852,
-  347.109,
-  4.948999999999984,
-  10.301000000000045
+    0.5,
+    0.5,
+    217.113,
+    16.4414,
+    14.168,
+    16.9414,
+    181.059,
+    19.414099999999998,
+    28.4297,
+    36.3555,
+    55.668000000000006,
+    10.695300000000003,
+    158.977,
+    36.3555,
+    7.920999999999992,
+    25.7539,
+    36.5547,
+    47.0508,
+    42.98440000000001,
+    15.058599999999998,
+    31.9961,
+    62.1094,
+    144.4099,
+    8.714800000000004,
+    23.2891,
+    70.8242,
+    159.10590000000002,
+    48.25779999999999,
+    15.918,
+    119.082,
+    83.43360000000001,
+    106.715,
+    20.1875,
+    225.797,
+    71.4023,
+    13.973000000000013,
+    102.844,
+    119.082,
+    85.37100000000001,
+    32.206999999999994,
+    102.844,
+    151.289,
+    89.25400000000002,
+    33.762,
+    24.9766,
+    239.77,
+    14.613199999999996,
+    19.456999999999965,
+    47.6055,
+    239.77,
+    11.7851,
+    13.561999999999983,
+    64.5781,
+    239.77,
+    27.01169999999999,
+    20.635999999999996,
+    66.4609,
+    260.406,
+    21.2149,
+    41.01600000000002,
+    69.2891,
+    301.422,
+    6.367199999999997,
+    15.557999999999993,
+    79.4258,
+    301.422,
+    6.6015000000000015,
+    25.456999999999994,
+    64.5781,
+    316.98,
+    12.492199999999997,
+    4.949999999999989,
+    75.6563,
+    326.879,
+    13.4375,
+    9.430000000000007,
+    75.3047,
+    336.309,
+    6.308599999999998,
+    9.069999999999993,
+    84.3086,
+    336.309,
+    5.960899999999995,
+    8.553999999999974,
+    52.375,
+    253.332,
+    5.257800000000003,
+    15.425999999999988,
+    21.1602,
+    259.227,
+    27.351499999999998,
+    7.675000000000011,
+    20.1875,
+    266.902,
+    5.609400000000001,
+    7.883000000000038,
+    28.5781,
+    266.902,
+    5.718800000000002,
+    14.012,
+    38.7773,
+    266.902,
+    4.636800000000001,
+    10.199999999999989,
+    22.9922,
+    280.914,
+    17.331999999999997,
+    5.149000000000001,
+    27.1875,
+    286.063,
+    5.093800000000002,
+    27.351,
+    27.1875,
+    313.414,
+    4.468800000000002,
+    10.819999999999993,
+    34.8359,
+    286.063,
+    9.351599999999998,
+    10.355000000000018,
+    35.5313,
+    296.418,
+    7.882799999999996,
+    7.879000000000019,
+    105.641,
+    185.051,
+    13.077999999999989,
+    11.426000000000016,
+    104.254,
+    196.477,
+    4.358999999999995,
+    6.100999999999999,
+    109.813,
+    196.477,
+    4.959999999999994,
+    19.429000000000002,
+    106.434,
+    215.906,
+    10.968000000000004,
+    6.2309999999999945,
+    109.813,
+    222.137,
+    3.600999999999999,
+    6.61699999999999,
+    181.543,
+    185.051,
+    12.843999999999994,
+    19.719000000000023,
+    183.684,
+    204.77,
+    5.753999999999991,
+    14.252999999999986,
+    179.27,
+    219.023,
+    15.11699999999999,
+    8.622000000000014,
+    180.207,
+    227.645,
+    4.546999999999997,
+    7.889999999999986,
+    189.438,
+    227.645,
+    4.414000000000016,
+    6.420999999999992,
+    123.215,
+    185.051,
+    20.335999999999984,
+    52.625,
+    137.531,
+    237.676,
+    9.937999999999988,
+    11.238,
+    127.766,
+    237.676,
+    8.159999999999982,
+    24.215000000000032,
+    152.379,
+    185.051,
+    23.414000000000016,
+    68.81200000000001,
+    156.125,
+    253.863,
+    14.38300000000001,
+    14.382999999999981,
+    159.605,
+    268.246,
+    9.497000000000014,
+    10.367000000000019,
+    121.609,
+    261.891,
+    19.132999999999996,
+    16.72199999999998,
+    116.395,
+    278.613,
+    59.39800000000001,
+    10.702999999999975,
+    166.961,
+    289.316,
+    12.308999999999997,
+    21.00400000000002,
+    170.508,
+    310.32,
+    5.284999999999997,
+    12.843999999999994,
+    113.414,
+    289.316,
+    11.941000000000003,
+    10.5,
+    118.668,
+    299.816,
+    5.35199999999999,
+    13.715000000000032,
+    115.59,
+    313.531,
+    10.432999999999993,
+    6.555000000000007,
+    131.176,
+    289.316,
+    30.703000000000003,
+    38.12900000000002,
+    130.305,
+    327.445,
+    14.581999999999994,
+    9.899000000000001,
+    150.641,
+    327.445,
+    14.984000000000009,
+    9.899000000000001,
+    137.598,
+    337.344,
+    22.74199999999999,
+    9.764999999999986,
+    138.867,
+    347.109,
+    4.6839999999999975,
+    14.180000000000007,
+    147.469,
+    347.109,
+    3.975999999999999,
+    10.301000000000045,
+    153.852,
+    347.109,
+    4.948999999999984,
+    10.301000000000045
 ];
 
 let dat;
@@ -254,137 +254,142 @@ let ground;
 let mouseConstraint;
 
 function preload() {
-  img = loadImage("dependency_2x.png", 
-    () => console.log("Image loaded successfully."),
-    (e) => {
-      console.warn("Could not load dependency_2x.png, using fallback placeholder.");
-      img = createImage(800, 600);
-      img.loadPixels();
-      for (let i = 0; i < img.width; i++) {
-        for (let j = 0; j < img.height; j++) {
-          img.set(i, j, color(30, 41, 59));
+    img = loadImage("dependency_2x.png",
+        () => console.log("Image loaded successfully."),
+        (e) => {
+            console.warn("Could not load dependency_2x.png, using fallback placeholder.");
+            img = createImage(800, 600);
+            img.loadPixels();
+            for (let i = 0; i < img.width; i++) {
+                for (let j = 0; j < img.height; j++) {
+                    img.set(i, j, color(30, 41, 59));
+                }
+            }
+            img.updatePixels();
         }
-      }
-      img.updatePixels();
-    }
-  );
+    );
 }
 
 // This sketch will use only rectangles.
 function createRect(x, y, w, h) {
-  const newBody = Matter.Bodies.rectangle(x, y, w, h);
-  Matter.Composite.add(engine.world, newBody);
+    const newBody = Matter.Bodies.rectangle(x, y, w, h);
+    Matter.Composite.add(engine.world, newBody);
 
-  colorMode(HSB, 255);
+    colorMode(HSB, 255);
 
-  return {
-    body: newBody,
-    w: w,
-    h: h,
-    fillColour: color(random(255), 150, 255),
-  };
+    return {
+        body: newBody,
+        w: w,
+        h: h,
+        fillColour: color(random(255), 150, 255),
+    };
 }
 
 function resetSketch() {
-  if (engine) {
-    Matter.World.clear(engine.world);
-    Matter.Engine.clear(engine);
-  }
-  
-  if (mouseConstraint && mouseConstraint.mouse) {
-    Matter.Mouse.clearSourceEvents(mouseConstraint.mouse);
-  }
-
-  engine = Matter.Engine.create();
-  engine.positionIterations = 1000;
-  engine.velocityIterations = 1000;
-
-  // Clone original data since mutates happen
-  dat = [...originalDat];
-  let ymax = 0;
-
-  for (let idx = 0; idx < dat.length; ++idx) {
-    dat[idx] *= 1.07;
-  }
-
-  for (let idx = 0; idx < dat.length; idx += 4) {
-    dat[idx + 1] = height - dat[idx + 1] - dat[idx + 3] - 20;
-    dat[idx] += 20;
-    let y = dat[idx + 1] + dat[idx + 3];
-    ymax = max(y, ymax);
-  }
-
-  rects = [];
-
-  // Create a "ground plane" at the bottom centre of the canvas
-  ground = createRect(width / 2, height, width, 2 * 31.535);
-  Matter.Body.setStatic(ground.body, true);
-
-  for (let idx = 0; idx < dat.length; idx += 4) {
-    const r = createRect(
-      dat[idx] + 0.5 * dat[idx + 2],
-      dat[idx + 1] + 0.5 * dat[idx + 3],
-      dat[idx + 2],
-      dat[idx + 3]
-    );
-    Matter.Body.setStatic(r.body, true);
-    rects.push(r);
-  }
-
-  Matter.Engine.update(engine);
-
-  // Virtual object that represents the mouse
-  const theMouse = Matter.Mouse.create(canvas.elt);
-  theMouse.pixelRatio = pixelDensity();
-  // A constraint that ties the mouse to bodies on demand
-  mouseConstraint = Matter.MouseConstraint.create(engine, {
-    mouse: theMouse,
-  });
-  // Add the constraint to the world
-  Matter.Composite.add(engine.world, mouseConstraint);
-  
-  Matter.Events.on(mouseConstraint, "mousedown", function () {
-    for (let r of rects) {
-      Matter.Body.setStatic(r.body, false);
+    if (engine) {
+        Matter.World.clear(engine.world);
+        Matter.Engine.clear(engine);
     }
-  });
+
+    if (mouseConstraint && mouseConstraint.mouse) {
+        Matter.Mouse.clearSourceEvents(mouseConstraint.mouse);
+    }
+
+    engine = Matter.Engine.create();
+    engine.positionIterations = 1000;
+    engine.velocityIterations = 1000;
+
+    // Clone original data since mutates happen
+    dat = [...originalDat];
+    let ymax = 0;
+
+    for (let idx = 0; idx < dat.length; ++idx) {
+        dat[idx] *= 1.07;
+    }
+
+    for (let idx = 0; idx < dat.length; idx += 4) {
+        dat[idx + 1] = height - dat[idx + 1] - dat[idx + 3] - 20;
+        dat[idx] += 20;
+        let y = dat[idx + 1] + dat[idx + 3];
+        ymax = max(y, ymax);
+    }
+
+    rects = [];
+
+    // Create a "ground plane" at the bottom centre of the canvas
+    ground = createRect(width / 2, height, width, 2 * 31.535);
+    Matter.Body.setStatic(ground.body, true);
+
+    for (let idx = 0; idx < dat.length; idx += 4) {
+        const r = createRect(
+            dat[idx] + 0.5 * dat[idx + 2],
+            dat[idx + 1] + 0.5 * dat[idx + 3],
+            dat[idx + 2],
+            dat[idx + 3]
+        );
+        Matter.Body.setStatic(r.body, true);
+        rects.push(r);
+    }
+
+    Matter.Engine.update(engine);
+
+    // Virtual object that represents the mouse
+    const theMouse = Matter.Mouse.create(canvas.elt);
+    theMouse.pixelRatio = pixelDensity();
+    // A constraint that ties the mouse to bodies on demand
+    mouseConstraint = Matter.MouseConstraint.create(engine, {
+        mouse: theMouse,
+    });
+    // Add the constraint to the world
+    Matter.Composite.add(engine.world, mouseConstraint);
+
+    Matter.Events.on(mouseConstraint, "mousedown", function () {
+        for (let r of rects) {
+            Matter.Body.setStatic(r.body, false);
+        }
+    });
 }
 
 function setup() {
-  // We use parent container for CSS styling
-  let container = select('#sketch-parent');
-  canvas = createCanvas(img.width ? img.width / 2 : 400, img.height ? img.height / 2 : 300);
-  canvas.parent(container);
+    // We use parent container for CSS styling
+    let container = select('#sketch-parent');
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent(container);
 
-  // Bind the reset button functionality
-  let resetBtn = select('#reset-btn');
-  if (resetBtn) {
-    resetBtn.mousePressed(resetSketch);
-  }
+    // Bind the reset button functionality
+    let resetBtn = select('#reset-btn');
+    if (resetBtn) {
+        resetBtn.mousePressed(resetSketch);
+    }
 
-  resetSketch();
+    resetSketch();
 }
 
 function draw() {
-  image(img, 0, 0, width, height);
+    image(img, 0, 0, width, height);
 
-  Matter.Engine.update(engine);
+    Matter.Engine.update(engine);
 
-  strokeWeight(2);
-  fill(225);
-  rectMode(CENTER);
+    strokeWeight(2);
+    fill(225);
+    rectMode(CENTER);
 
-  for (let re of rects) {
-    push();
-    translate(re.body.position.x, re.body.position.y);
-    rotate(re.body.angle);
-    rect(0, 0, re.w, re.h, 2);
-    pop();
-  }
+    for (let re of rects) {
+        push();
+        translate(re.body.position.x, re.body.position.y);
+        rotate(re.body.angle);
+        rect(0, 0, re.w, re.h, 2);
+        pop();
+    }
 
-  rectMode(CORNER);
-  noFill();
-  strokeWeight(4);
-  stroke(100); // subtle border around the canvas instead of black on black
-  rect(0, 0, width, height);
+    rectMode(CORNER);
+    noFill();
+    strokeWeight(4);
+    stroke(100); // subtle border around the canvas instead of black on black
+    rect(0, 0, width, height);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    resetSketch();
 }
